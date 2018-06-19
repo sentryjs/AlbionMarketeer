fcbeb1ef00fe1484fe78bb19c135c54b98869f62
sentryjs <joshua.d.mcdonald@outlook.com>
2018-06-19 00:44:52 -0500

    Updates to #2
    Better than it was...

--- AlbionMarketeer/MainWindow.xaml.cs
+++ AlbionMarketeer/MainWindow.xaml.cs
@@ -31,6 +31,11 @@ namespace AlbionMarketeer
         public static Log log_window;
         public static DataSet itemsds = new DataSet();
 
+        public static Dictionary<string, string> Result;
+        public static List<string> Item_keys = new List<string>();
+        public static List<string> Item_values = new List<string>();
+        public static List<ApiOrder> apiOrders = new List<ApiOrder>();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -47,10 +52,10 @@ namespace AlbionMarketeer
             return AppDomain.CurrentDomain.BaseDirectory;
         }
 
-        public Task<List<string>> FindItems(string search)
+        public Task<Dictionary<string, string>> FindItems(string search)
         {
-            var taskCompletionSource = new TaskCompletionSource<List<string>>();
-            Task<List<string>> task = Task.Factory.StartNew(() =>
+            var taskCompletionSource = new TaskCompletionSource<Dictionary<string, string>>();
+            Task<Dictionary<string, string>> task = Task.Factory.StartNew(() =>
             {
                 string xmlLocalization = "localization.xml";
 
@@ -59,7 +64,7 @@ namespace AlbionMarketeer
 
                 DataTableCollection tables = itemsds.Tables;
 
-                List<string> found = new List<string>();
+                Dictionary<string, string> found = new Dictionary<string, string>();
 
                 foreach (DataTable table in tables)
                 {
@@ -71,7 +76,7 @@ namespace AlbionMarketeer
                         {
                             foreach (var parent in tables["tu"].AsEnumerable())
                             {
-                                if (row[2].ToString() == parent[0].ToString() && parent[1].ToString().StartsWith("@ITEMS_")) found.Add(parent[1].ToString().Substring(7));
+                                if (row[2].ToString() == parent[0].ToString() && parent[1].ToString().StartsWith("@ITEMS_")) found.Add(parent[1].ToString().Substring(7), row[0].ToString());
                             }
                         }
                     }
@@ -127,31 +132,30 @@ namespace AlbionMarketeer
 
             string Locations_joined = string.Join(",", Locations);
 
-            List<string> Items;
             var task = FindItems(search.Text);
-            task.ContinueWith(t =>
+            task.ContinueWith(async t =>
             {
-                Items = t.Result;
-                string Items_joined = string.Join(",", Items);
-                return $"https://www.albion-online-data.com/api/v1/stats/prices/{Items_joined}?locations={Locations_joined}";
-            }).ContinueWith(async t =>
-            {
+                Result = t.Result;
+
+                Item_keys.AddRange(Result.Keys);
+                Item_values.AddRange(Result.Values);
+
+                string Items_joined = string.Join(",", Item_keys);
+                string url = $"https://www.albion-online-data.com/api/v1/stats/prices/{Items_joined}?locations={Locations_joined}";
+
                 string result = "";
-                result = await GetAPIData(t.Result);
+                result = await GetAPIData(url);
 
-                dynamic obj = JsonConvert.DeserializeObject<dynamic>(result);
+                //dynamic obj = JsonConvert.DeserializeObject<dynamic>(result);
+                apiOrders = ApiOrder.FromJson(result);
 
-                grid.ItemsSource = obj;
+                foreach (var pair in Result)
+                {
+                    ApiOrder order = apiOrders.Find(o => o.ItemId == pair.Key);
+                    if (order != null) order.Name = pair.Value;
+                }
 
-                Dispatcher.Invoke(new Action(() => loading.Content = ""));
-                Dispatcher.Invoke(new Action(() => caerleon.IsEnabled = true));
-                Dispatcher.Invoke(new Action(() => bridgewatch.IsEnabled = true));
-                Dispatcher.Invoke(new Action(() => martlock.IsEnabled = true));
-                Dispatcher.Invoke(new Action(() => thetford.IsEnabled = true));
-                Dispatcher.Invoke(new Action(() => sterling.IsEnabled = true));
-                Dispatcher.Invoke(new Action(() => lymhurst.IsEnabled = true));
-                Dispatcher.Invoke(new Action(() => search.IsEnabled = true));
-                Dispatcher.Invoke(new Action(() => search_button.IsEnabled = true));
+                Results.ItemsSource = apiOrders.Select(c => c.Name).Where(s => s != null).ToList();
             }, TaskScheduler.FromCurrentSynchronizationContext());
             
         }
@@ -162,6 +166,15 @@ namespace AlbionMarketeer
             log_window.Dispatcher.Invoke(new Action(() => log_window.AddLog(response.ReasonPhrase.ToString())));
             if (response.IsSuccessStatusCode)
             {
+                Dispatcher.Invoke(new Action(() => loading.Content = ""));
+                Dispatcher.Invoke(new Action(() => caerleon.IsEnabled = true));
+                Dispatcher.Invoke(new Action(() => bridgewatch.IsEnabled = true));
+                Dispatcher.Invoke(new Action(() => martlock.IsEnabled = true));
+                Dispatcher.Invoke(new Action(() => thetford.IsEnabled = true));
+                Dispatcher.Invoke(new Action(() => sterling.IsEnabled = true));
+                Dispatcher.Invoke(new Action(() => lymhurst.IsEnabled = true));
+                Dispatcher.Invoke(new Action(() => search.IsEnabled = true));
+                Dispatcher.Invoke(new Action(() => search_button.IsEnabled = true));
                 return await response.Content.ReadAsStringAsync();
             }
             else
@@ -186,6 +199,20 @@ namespace AlbionMarketeer
             {
                 SearchAsync(new object(), new RoutedEventArgs());
             }
+        }
+
+        private void Results_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (Results.Items.Count <= 0) return;
+
+            int index = Item_values.FindIndex(i => i == Results.SelectedItem.ToString());
+            ShowDetails(Item_keys.ElementAt(index));
+        }
+
+        private void ShowDetails(string key)
+        {
+            Details details_window = new Details(apiOrders.Find(o => o.ItemId == key));
+            details_window.Show();
         }
     }
 }
