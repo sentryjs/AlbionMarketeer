b799114fcdcf72cf00f84bdc61426b6b93b5915e
sentryjs <joshua.d.mcdonald@outlook.com>
2018-06-29 17:21:37 -0500

    Removed internal and TPC folders

--- AlbionMarketeer/Output/Sign.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-Pushd C:\Users\joshu\source\repos\AlbionMarketeer\AlbionMarketeer\Output
-
-"c:\Program Files (x86)\Windows Kits\10\bin\x86\signtool.exe" sign /f VigilGaming.pfx /p !!JJmm159753 /t http://timestamp.verisign.com/scripts/timstamp.dll "AlbionMarketeerSetup.exe"
